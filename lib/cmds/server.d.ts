e3cbe4740d8f4fb364bc4c2147cd850f3d97bf6b
Pablo Schaffner <pablo@puntorigen.com>
2022-05-11 13:49:30 -0400

    added parser improved file exists detection

--- lib/cmds/server.d.ts
+++ lib/cmds/server.d.ts
@@ -1,6 +1,7 @@
 import Command from '../common/command';
 export default class Server extends Command {
     init(): Promise<boolean>;
+    createPresentation(): Promise<void>;
     process(): Promise<void>;
 }
 //# sourceMappingURL=server.d.ts.map
--- src/cmds/server.ts
+++ src/cmds/server.ts
@@ -1,4 +1,7 @@
 import Command from '../common/command'
+const marked = require('marked').marked;
+const fs = require('fs').promises;
+let sourceFile = '';
 
 //starts a local web server with the presentation
 export default class Server extends Command {
@@ -11,19 +14,32 @@ export default class Server extends Command {
             '@':'green',
             '!':'brightRed'
         });
-        return true;
-    }
-
-    async process() {
         let file = '';
         if (this.arg._.length==0) file = await this.ask(`Please enter the filename for the DB backup:`);
         if (this.arg._.length>0) file = this.arg._.shift();
+        const path = require('path');
+        const fs = require('fs').promises;
+        sourceFile = path.join(process.cwd(),file);
+        try {
+            await fs.stat(sourceFile)
+            return true;
+        } catch {
+            this.log(`Error: the given file doesn't exist`);
+            return false;
+        }
+    }
+
+    async createPresentation() {
+        const mdSource = await fs.readFile(sourceFile,{ encoding:'utf-8' });
+        let parsed = marked.lexer(mdSource);
+        console.log(parsed);
+    }
+
+    async process() {
+        await this.createPresentation();
         /*
         const root = require('find-root')(__dirname)
-        const path = require('path');
         const source = path.join(root,'db.json')
-        const target = path.join(process.cwd(),file);
-        const fs = require('fs').promises;
         await fs.copyFile(source,target);
         this.log('DB backup written successfully!','green')*/
     }
